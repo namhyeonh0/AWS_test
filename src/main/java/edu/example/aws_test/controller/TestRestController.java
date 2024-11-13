f0d60424d5fef1943e7cc8e99b01979ee5acfc7e
namhyeonh0 <www101365@naver.com>
2024-11-13 09:42:39 +0900

    branch commit and push test

--- src/main/java/edu/example/aws_test/controller/TestRestController.java
+++ src/main/java/edu/example/aws_test/controller/TestRestController.java
@@ -1,11 +1,8 @@
 package edu.example.aws_test.controller;
 
-import edu.example.aws_test.service.TestService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 public class TestRestController {
-    private final TestService testService;
+
 }
