eb8356cd963711d8c4ab1f40ceab550f769de629
namhyeonh0 <www101365@naver.com>
2024-11-13 09:37:11 +0900

    db 변경 후 test

--- src/main/java/edu/example/aws_test/controller/TestRestController.java
+++ src/main/java/edu/example/aws_test/controller/TestRestController.java
@@ -2,11 +2,9 @@ package edu.example.aws_test.controller;
 
 import edu.example.aws_test.service.TestService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/tests")
 @RequiredArgsConstructor
 public class TestRestController {
     private final TestService testService;
